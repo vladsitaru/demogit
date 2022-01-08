8df4ae670d0b7bf4d6be6c4061bc9a0e39c8aa79
VladLenovoPC <vladsitaru@outlook.com>
2022-01-08 15:22:49 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // RUN - RIGHT - FAST
+
+        // Book (String title, Autor autor)
+        // Autor (String nume, boolean preferat)
+        // Wishlist (Book book, booleam am)
+        // TrackingReading (Book book, boolean isfromwishlist, wishlistid)
+
+
+    }
+}
