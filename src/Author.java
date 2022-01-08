727bba48687ab6b1e8aadbab884b048a9d0afc95
VladLenovoPC <vladsitaru@outlook.com>
2022-01-08 15:26:44 +0200

    first version

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,20 @@
 public class Author {
+    private String name;
+    private boolean isPreferred;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPreferred() {
+        return isPreferred;
+    }
+
+    public void setPreferred(boolean preferred) {
+        isPreferred = preferred;
+    }
 }
